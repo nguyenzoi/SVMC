211a2afe62a2fb94403df793cfabc91a95dec1d5
NguNgu <lenguyenk57@gmail.com>
2021-12-03 21:24:53 +0700

    Done video #3

---
+++ app/src/main/java/com/svmc/exampleapplication/nguyenlv/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.svmc.exampleapplication.nguyenlv.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.svmc.exampleapplication.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
