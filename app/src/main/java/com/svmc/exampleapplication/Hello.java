4debf943c120d802e1b9ee0c5a3cba0e877efcc2
Ba <luckyba2010@gmail.com>
2021-12-07 23:25:58 +0700

    #2 Layouts & Room Entity

--- app/src/main/java/com/svmc/exampleapplication/Hello.java
+++
@@ -1,4 +0,0 @@
-package com.svmc.exampleapplication;
-
-public class Hello {
-}
---
+++ app/src/main/java/com/svmc/exampleapplication/luantv/data/Task.kt
@@ -0,0 +1,20 @@
+package com.svmc.exampleapplication.luantv.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "task_table")
+@Parcelize
+data class Task(
+    val name: String,
+    val important: Boolean = false,
+    val completed: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+): Parcelable {
+    val createdDateFormat: String
+        get() = DateFormat.getDateTimeInstance().format(created)
+}
