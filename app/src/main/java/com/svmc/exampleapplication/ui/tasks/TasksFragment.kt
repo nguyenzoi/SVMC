9334630d3c2efceaace8ca5c69be2dac464615e7
NguNgu <lenguyenk57@gmail.com>
2021-12-14 18:30:48 +0700

    Navigation Component #3

---
+++ app/src/main/java/com/svmc/exampleapplication/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.svmc.exampleapplication.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.svmc.exampleapplication.R
+
+class TasksFragment : Fragment(R.layout.task_fragment) {
+}
