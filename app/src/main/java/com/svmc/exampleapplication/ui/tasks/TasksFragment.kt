a78c703c2e198a80e4369f80321ca125eaf1d61d
NguNgu <lenguyenk57@gmail.com>
2021-12-14 19:44:49 +0700

    Room Dependency Injection #4

--- app/src/main/java/com/svmc/exampleapplication/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/svmc/exampleapplication/ui/tasks/TasksFragment.kt
@@ -1,7 +1,11 @@
 package com.svmc.exampleapplication.ui.tasks
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.svmc.exampleapplication.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.task_fragment) {
+    private val viewModel: TaskViewModel by viewModels()
 }
