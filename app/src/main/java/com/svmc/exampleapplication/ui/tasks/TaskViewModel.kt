f8377a64e1bf10750d725e722b08557656a9e7a9
NguNgu <lenguyenk57@gmail.com>
2021-12-14 20:28:05 +0700

    ListAdapter with View Binding #5

--- app/src/main/java/com/svmc/exampleapplication/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/svmc/exampleapplication/ui/tasks/TaskViewModel.kt
@@ -2,9 +2,11 @@ package com.svmc.exampleapplication.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.svmc.exampleapplication.data.TaskDao
 
 class TaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+    val task = taskDao.getTasks().asLiveData()
 }
