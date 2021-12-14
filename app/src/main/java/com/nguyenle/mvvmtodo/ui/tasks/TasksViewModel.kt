b98e91935cbeefcc1a79a73c5560eb903dd78efe
NguNgu <lenguyenk57@gmail.com>
2021-12-14 23:11:42 +0700

    Room Search #6

--- app/src/main/java/com/nguyenle/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/nguyenle/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -4,9 +4,16 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.nguyenle.mvvmtodo.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
-    val task = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+    private val taskFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+    val task = taskFlow.asLiveData()
+
 }
