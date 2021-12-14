ebd5ad197028059ea9d5fb518eb715efe90da92f
Ba <luckyba2010@gmail.com>
2021-12-14 22:54:15 +0700

    #12 Add/Edit Logic

--- app/src/main/java/com/svmc/exampleapplication/luantv/ui/editor/EditFragment.kt
+++ app/src/main/java/com/svmc/exampleapplication/luantv/ui/editor/EditFragment.kt
@@ -2,13 +2,20 @@ package com.svmc.exampleapplication.luantv.ui.editor
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.svmc.exampleapplication.R
 import com.svmc.exampleapplication.databinding.EditFragmentMvvmBinding
+import com.svmc.exampleapplication.luantv.util.exhaustive
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.item_task_mvvm.*
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class EditFragment: Fragment(R.layout.edit_fragment_mvvm) {
@@ -25,6 +32,36 @@ class EditFragment: Fragment(R.layout.edit_fragment_mvvm) {
             checkBookImportance.jumpDrawablesToCurrentState()
             tvCreatedDate.isVisible = viewModel.task != null
             tvCreatedDate.text = viewModel.task?.createdDateFormat
+
+            edtTaskTitle.addTextChangedListener {
+                viewModel.taskName = it.toString()
+            }
+
+            checkBookImportance.setOnCheckedChangeListener { _, isChecked ->
+                viewModel.taskImportance = isChecked
+            }
+
+            bntSave.setOnClickListener {
+                viewModel.onSaveClicked()
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            viewModel.editTaskEvent.collect { event ->
+                when(event) {
+                    is EditViewModel.AddEditTaskEvent.NotifyInvalidValue -> {
+                        Snackbar.make(requireView(), event.text, Snackbar.LENGTH_LONG).show()
+                    }
+                    is EditViewModel.AddEditTaskEvent.NavigateBackWithResult ->  {
+                        binding.edtTaskTitle.clearFocus()
+                        setFragmentResult(
+                            "add_update_request",
+                            bundleOf("add_edit_result" to event.result)
+                        )
+                        findNavController().popBackStack()
+                    }
+                }.exhaustive
+            }
         }
     }
 }
--- app/src/main/java/com/svmc/exampleapplication/luantv/ui/editor/EditViewModel.kt
+++ app/src/main/java/com/svmc/exampleapplication/luantv/ui/editor/EditViewModel.kt
@@ -4,13 +4,22 @@ import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.svmc.exampleapplication.luantv.data.Task
 import com.svmc.exampleapplication.luantv.data.TaskDao
+import com.svmc.exampleapplication.luantv.ui.ADD_TASK_RESULT_OK
+import com.svmc.exampleapplication.luantv.ui.EDIT_TASK_RESULT_OK
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class EditViewModel @ViewModelInject constructor(
     val taskDao: TaskDao,
     @Assisted val state: SavedStateHandle
 ): ViewModel(){
+    private val editTaskEventChannel = Channel<AddEditTaskEvent>()
+    val editTaskEvent = editTaskEventChannel.receiveAsFlow()
+
     val task = state.get<Task>("task")
     var taskName = state.get<String>("taskName")?:task?.name?:""
         set(value) {
@@ -20,4 +29,31 @@ class EditViewModel @ViewModelInject constructor(
         set(value) {
             field = value
         }
+
+    fun onSaveClicked () = viewModelScope.launch {
+        if (taskName.isBlank()) {
+            editTaskEventChannel.send(AddEditTaskEvent.NotifyInvalidValue("Name is wrong. Please type again"))
+        } else {
+            if (task != null) {
+                updateTask(task.copy(name = taskName, important = taskImportance))
+            } else {
+                createTask(Task(taskName, taskImportance))
+            }
+        }
+    }
+
+    private fun createTask(task: Task) = viewModelScope.launch {
+        taskDao.insert(task)
+        editTaskEventChannel.send(AddEditTaskEvent.NavigateBackWithResult(ADD_TASK_RESULT_OK))
+    }
+    private fun updateTask(task: Task) = viewModelScope.launch {
+        taskDao.update(task)
+        editTaskEventChannel.send(AddEditTaskEvent.NavigateBackWithResult(EDIT_TASK_RESULT_OK))
+    }
+
+    sealed class AddEditTaskEvent {
+
+        data class NotifyInvalidValue(val text: String): AddEditTaskEvent()
+        data class NavigateBackWithResult(val result: Int): AddEditTaskEvent()
+    }
 }
